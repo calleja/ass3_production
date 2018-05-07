4e2f65e753504d3fcacfc2a6e550d3d744a6401f
calleja <lecn99@hotmail.com>
2018-05-07 04:44:20 -0400

    more

--- run_cryptoApp.py
+++ run_cryptoApp.py
@@ -6,6 +6,4 @@ from app import app
 
 if __name__ == '__main__':
     app.debug = True
-    host = os.environ.get('IP', '127.0.0.1')
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host=host, port=port) #spins up a server and waits for request from client
+    app.run(host='0.0.0.0') #spins up a server and waits for request from client
